cabe4a06470bcd3037efa3c847cddd64b6cba1b4
Vinicius Bueno Costa <buenov535@gmail.com>
2023-11-30 13:43:15 -0300

    final commit

--- src/components/PasswordListItems/CopyToClipboard.tsx
+++ src/components/PasswordListItems/CopyToClipboard.tsx
@@ -1,5 +1,7 @@
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 
 type CopyToClipboardProps = {
   textToCopy: string,
@@ -7,16 +9,31 @@ type CopyToClipboardProps = {
 };
 
 function CopyToClipboard({ textToCopy, hidden = false }: CopyToClipboardProps) {
+  const [wasCopied, setWasCopied] = useState(false);
+
+  setTimeout(() => { setWasCopied(false); }, 400);
+  function handleClick(text: string) {
+    navigator.clipboard.writeText(text);
+    setWasCopied(true);
+  }
   return (
     <button
-      onClick={ () => { navigator.clipboard.writeText(textToCopy); } }
+      onClick={ () => { handleClick(textToCopy); } }
       aria-label="copy to clipboard"
     >
       <span
         className="bg-zinc-800 py-2 px-5 rounded-lg
     flex justify-between cursor-pointer items-center"
       >
-        {hidden ? '******' : textToCopy}
+        {
+        wasCopied && (
+          <span className="text-green-300 flex gap-2 items-center">
+            <FontAwesomeIcon icon={ faCheck } />
+            Copiado
+          </span>
+        )
+}
+        {!wasCopied && (hidden ? '******' : textToCopy)}
         <FontAwesomeIcon icon={ faCopy } />
       </span>
     </button>
